1d56bb254225ee492674244cbd7f2c4ffc9bcfc1
mramirez92 <ramirez.92.monica@gmail.com>
2023-04-25 21:43:48 -0500

    table testing added

--- blackjack/blackjack_test.go
+++ blackjack/blackjack_test.go
@@ -2,12 +2,46 @@ package blackjack
 
 import "testing" 
 
-func TestStrToInt(t *testing.T){
+func TestParseCardNotFound(t *testing.T){
 
-	got := ParseCard("seven")
-	want := 7
+	got := ParseCard("twenty")
+	want := -1
 
 	if got != want{
 		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestParseCard(t *testing.T){
+	cases := []struct{
+		card string
+		expected int
+	}{
+		{
+			card: "nine",
+			expected: 9 ,
+		},
+		{
+			card: "ace", 
+			expected: 11,
+
+		},
+		{
+			card: "king",
+			expected: 10,
+
+		},
+		{
+			card: "two", 
+			expected: 2,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.card, func(t *testing.T){
+			result := ParseCard(tc.card)
+			if result != tc.expected{
+				t.Errorf(" expected %q, but got %q", tc.expected, result)
+			}
+		})
 	}
 }
