512e9b4a4b06cd14be1d1f3a86a44795b2355d46
mramirez92 <ramirez.92.monica@gmail.com>
2023-05-11 19:01:24 -0500

    leetcode 724 find pivot index

--- leetcode/leetcode.go
+++ leetcode/leetcode.go
@@ -9,3 +9,20 @@ func IsSubsequence(s, t string) bool{
 	}
 	return i == len(s)
 }
+
+func FindPivot(nums [] int) int{
+	left, right := 0 , 0
+
+	for _, num := range nums { 
+		right += num 
+	}
+
+	for i, num := range nums {
+		right -= num 
+		if right == left{
+			return i
+		}
+		left += num 
+	}
+	return -1
+}
--- leetcode/leetcode_test.go
+++ leetcode/leetcode_test.go
@@ -31,3 +31,30 @@ func TestIsSub(t *testing.T){
 		}
 	}
 }
+
+func TestFindPivot(t *testing.T) {
+	cases := [] struct {
+		nums []int
+		expected int
+	}{
+		{
+			nums: []int {5, 2, 3, 4, 3}, 
+			expected: 2,
+		},
+		{
+			nums: []int {2, 1, -1}, 
+			expected: 0,
+		},
+		{
+			nums: []int {2,7},
+			expected: -1,
+		},
+	}
+	for _, tc := range cases{
+		results := FindPivot(tc.nums)
+		if results != tc.expected { 
+			t.Errorf("got %+v, expected %+v", results, tc.expected)
+		}
+	}
+
+}
