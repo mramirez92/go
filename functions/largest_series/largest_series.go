750759b7849680e0e2edae606acd6be9297fa721
mramirez92 <ramirez.92.monica@gmail.com>
2023-07-03 10:13:48 -0500

    misread instructions fixed accordingly

--- functions/largest_series/largest_series.go
+++ functions/largest_series/largest_series.go
@@ -1,47 +1,38 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
-	"errors"
+	"strings"
 )
 
-func Series(digits string, span int) (int64, error) {
-	switch{
-	case span <0:
+func Series(digits string, span int) (current int64, e error) {
+	if span < 0 {
 		return 0, errors.New("span must not be negative")
-	case span >len(digits):
+	} else if span > len(digits) {
 		return 0, errors.New("span must be smaller than string length")
 	}
-	series := []string{}
-	for i := 0; i <= len(digits)-span; i++ {
-		series = append(series, digits[i:i+span])
-	}
-	return Largest(series)
-}
+	nums := strings.Split(digits, "")
 
-func Largest(nums []string) (current int64, e error) {
-	lgProduct :=  0
-
-	for _, num := range nums {
-		product := 1
-		for _, char := range num {
-			if num, e := strconv.Atoi(string(char)); e != nil{
+	for i := 0; i <= len(nums)-span; i++ {
+		var product int64 = 1
+		for j := 0; j < span; j++ {
+			if num, e := strconv.Atoi(nums[i+j]); e != nil {
 				return 0, errors.New("digits input must only contain digits")
-			}else{
-				product *= num
+			} else {
+				product *= int64(num)
 			}
 		}
-		if product > lgProduct {
-			n, _ := strconv.Atoi(num)
-			current, lgProduct = int64(n), product
+		if product > current {
+			current = product
 		}
 	}
 	return current, e
 }
 
 func main() {
-	nums := "12345"
-	fmt.Println(Series(nums, -1))
-	
+	nums := "29"
+	fmt.Println(Series(nums, 2))
+
 }
