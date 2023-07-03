7489a25a5110e6f716c6b97784eef505db89b8df
mramirez92 <ramirez.92.monica@gmail.com>
2023-07-03 14:55:26 -0500

    added atabash cipher, incomplete

--- functions/cipher/cipher.go
+++ functions/cipher/cipher.go
@@ -5,14 +5,14 @@ import (
 	"unicode"
 )
 
-func Cipher(str string, key int) (code string) {
+func Ceasar(str string, key int) (code string) {
 	shift := key % 26
 	for _, char := range str {
 		if unicode.IsLetter(char) {
 			new := char + rune(shift)
 			if unicode.IsLower(char) && new > 122 || unicode.IsUpper(char) && new > 90 {
 				new -= 26
-			} 
+			}
 			code += string(new)
 		} else {
 			code += string(char)
@@ -21,8 +21,35 @@ func Cipher(str string, key int) (code string) {
 	return code
 }
 
+func Atbash(str string) []string {
+	coded := []string{}
+	new := ""
+	for _, char := range str {
+			if len(new) < 5{
+				if unicode.IsLetter(char) {
+					r := 122 - (int(char) - 97)
+					new += string(rune(r))
+				} else if unicode.IsNumber(char) {
+					new += string(char)
+				}
+			}
+			if len(new) == 5{
+				coded = append(coded, new)
+				new =""
+			}
+			}
+			if len(new) > 0 {
+				coded = append(coded, new)
+			}
+			return coded
+		}
+
+
+
 func main() {
-	s := "abcz ZX-12!ab3"
-	fmt.Println(Cipher(s, 26))
+	s := "ab1 zyx 5678,"
+	// fmt.Println(Cipher(s, 26))
+
+	fmt.Println(Atbash(s))
 
 }
