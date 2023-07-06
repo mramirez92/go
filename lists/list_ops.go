8246300623cecdb554e27fc18cffe7b3390ba2fb
mramirez92 <ramirez.92.monica@gmail.com>
2023-07-06 13:31:01 -0500

    list ops complete, rough

--- lists/list_ops.go
+++ lists/list_ops.go
@@ -1,35 +1,53 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
+// IntList is an abstraction of a list of integers which we can define methods on
 type IntList []int
 
-// **foldl
-
-//**foldr
-
-// ?
-func (s IntList) Filter(fn func(int) bool) (match IntList) {
-	odd := func(n int) bool {
-		return n%2 == 1
+func (s IntList) Foldl(fn func(int, int) int, initial int) int {
+	for _, v := range s {
+		initial = fn(initial, v)
 	}
-	for _, num := range s {
-		if odd(num) {
-			match = append(match, num)
+	return initial
+}
+
+func (s IntList) Foldr(fn func(int, int) int, initial int) int {
+	for i := s.Length() - 1; i >= 0; i-- {
+		initial = fn(s[i], initial)
+	}
+	return initial
+}
+
+func (s IntList) Filter(fn func(int) bool) IntList {
+	res := IntList{}
+	for _, v := range s {
+		if fn(v) {
+			res = append(res, v)
 		}
 	}
-	return
+	return res
 }
 
 func (s IntList) Length() int {
 	return len(s)
 }
 
-// ** map
+func (s IntList) Map(fn func(int) int) IntList {
+	res := IntList{}
+	for _, v := range s {
+		res = append(res, fn(v))
+	}
+	return res
+}
 
-// ** reverse**
+func (s IntList) Reverse() IntList {
+	res := IntList{}
+	for i := len(s) - 1; i >= 0; i-- {
+		res = append(res, s[i])
+	}
+	return res
+}
 
 func (s IntList) Append(lst IntList) IntList {
 	return append(s, lst...)
@@ -42,17 +60,17 @@ func (s IntList) Concat(lists []IntList) IntList {
 	return s
 }
 
-// type.method(changes?)
-
 func main() {
-	s := []IntList{[]int{3}, []int{}, []int{4, 5, 6}}
-	n := IntList{1}
+	// s := []IntList{[]int{3}, []int{}, []int{4, 5, 6}}
+	s := IntList{1, 2}
 
 	// fmt.Println(s.Append(n))
-	// fmt.Println(s.Filter(func(i int) bool {
-	// 	return i %2 ==1
-	// }))
+	fmt.Println(s.Filter(func(i int) bool {
+		return i%2 == 0
+	}))
 
-	fmt.Println(n.Concat(s))
+	fmt.Println(s.Map(func(i int) int {
+		return i + 1
+	}))
 
 }
