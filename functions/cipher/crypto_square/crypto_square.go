01d2f58f0b988e0c65c57efe7830f5b298c251d4
mramirez92 <ramirez.92.monica@gmail.com>
2023-07-11 14:58:33 -0500

    split function implemented, splitting correctly, modification needed

--- functions/cipher/crypto_square/crypto_square.go
+++ functions/cipher/crypto_square/crypto_square.go
@@ -9,23 +9,39 @@ import (
 
 func filter(s string) string {
 	s = strings.ReplaceAll(strings.ToLower(s), " ", "")
-	// reg := regexp.MustCompile(`[^a-zA-Z0-9 ]+`)
-	// filtered := reg.ReplaceAllString(s, "")
 	return regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(s, "")
 }
 
 func sides(n int) (c, r int) {
 	square := int(math.Sqrt(float64(n)))
-	c, r = square, square
+	c, r = square, square 
 	if (c * r) >= n {
 		return
 	}
 	return c + 1, r
 }
 
-func main() {
-	// input := "Hello! 123, "
+func split(s string) []string {
+	st := []string{}
+	s = filter(s)
+	c, _ := sides(len(s))
 
-	// fmt.Println(filter(input)) // Output: Hello123
-	fmt.Println(sides(54))
+	for i:= 0; i < c ; i++{
+		current := ""
+		for j := i; j <= len(s)-1; j+=c{
+			current+=string(s[j])
+		}
+		st = append(st, current)
+	}
+	return st
+}
+
+func main() {
+	input := "123456789a"
+	// l :=filter(input)
+	// fmt.Println(len(l))
+	// fmt.Println(sides(len(l)))
+	fmt.Println(split(input))
+
+	
 }
