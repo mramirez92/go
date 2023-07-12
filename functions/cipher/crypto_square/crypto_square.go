846860b1dc8556e93a95b5348dec19c1ec50f5fe
mramirez92 <ramirez.92.monica@gmail.com>
2023-07-11 22:03:01 -0500

    refactored crypto_square, removed filter func, implemented regex and string filtering to Encode func

--- functions/cipher/crypto_square/crypto_square.go
+++ functions/cipher/crypto_square/crypto_square.go
@@ -7,21 +7,17 @@ import (
 	"strings"
 )
 
-func filter(s string) string {
-	s = strings.ReplaceAll(strings.ToLower(s), " ", "")
-	return regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(s, "")
-}
-
-func sides(n int) (c, r int) {
+func ColsRows(n int) (c, r int) {
 	c = int(math.Ceil(math.Sqrt(float64(n))))
 	r = int(math.Ceil(float64(n) / float64(c)))
 	return c, r
 }
 
-func split(s string) []string {
+func Encode(s string) string {
 	st := []string{}
-	s = filter(s)
-	c, r := sides(len(s))
+	s = strings.ReplaceAll(strings.ToLower(s), " ", "")
+	s = regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(s, "")
+	c, r := ColsRows(len(s))
 
 	for i:= 0; i < c ; i++{
 		current := ""
@@ -33,15 +29,10 @@ func split(s string) []string {
 		}
 		st = append(st, current)
 	}
-	return st
+	return strings.Join(st, " ")
 }
 
 func main() {
-	input := "1"
-	// l :=filter(input)
-	// fmt.Println(len(l))
-	fmt.Println(sides(len(input)))
-	fmt.Println(split(input))
-
-	
+	input := "See you later alligator!"
+	fmt.Println(Encode(input))	
 }
