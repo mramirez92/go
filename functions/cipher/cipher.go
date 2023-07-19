5f9221a729598231f22c50958f6a0bbc985bee30
mramirez92 <ramirez.92.monica@gmail.com>
2023-07-19 10:34:05 -0500

    more type/method practice

--- functions/cipher/cipher.go
+++ functions/cipher/cipher.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 	"unicode"
 )
+
+type shift int
+type vigenere string
+type Cipher interface {
+	Encode(string) string
+	Decode(string) string
+}
 
 func Ceasar(str string, key int) (code string) {
 	shift := key % 26
@@ -46,10 +54,97 @@ func Atbash(s string) string {
 	return strings.Join(coded, " ")
 }
 
-func main() {
-	s := "ab1 zyx 5678,"
-	// fmt.Println(Cipher(s, 26))
+func (c shift) Encode(input string) string {
+	input = strings.ToLower(strings.ReplaceAll(input, " ", ""))
+	new := ""
+	for _, char := range input {
+		if unicode.IsLetter(char) {
+			char += rune(c)
+			if char > 122 {
+				char = (char - 122) + 96
+				new += string(char)
+			} else if char < 97 {
+				char = 123 - (97 - char)
+				new += string(char)
+			} else {
+				new += string(char)
+			}
+		}
+	}
+	return new
+}
 
-	fmt.Println(Atbash(s))
+func NewShift(distance int) Cipher {
+	if distance == 0 || distance > 25 || distance < -25 {
+		return nil
+	}
+	return shift(distance)
+}
+func (c shift) Encode(input string) (new string) {
+	input = strings.ToLower(strings.ReplaceAll(input, " ", ""))
+	for _, char := range input {
+		if unicode.IsLetter(char) {
+			char += rune(c)
+			if char > 122 {
+				char = (char - 122) + 96
+				new += string(char)
+			} else if char < 97 {
+				char = 123 - (97 - char)
+				new += string(char)
+			} else {
+				new += string(char)
+			}
+		}
+	}
+	return new
+}
+
+func (c shift) Decode(input string) string {
+	decodeShift := shift(-int(c))
+	return decodeShift.Encode(input)
+}
+
+func NewVigenere(key string) Cipher {
+	alphaOnly := regexp.MustCompile("[^a-z]").ReplaceAllString(key, "")
+	if key != strings.ToLower(key) || strings.Count(key, "a") == len(key) || len(alphaOnly) != len(key) {
+		return nil
+	}
+	return vigenere(key)
+}
+
+func (v vigenere) Encode(input string) (new string) {
+	result := regexp.MustCompile("[^a-z]").ReplaceAllString(strings.ToLower(input), "")
+	keyLen := len(v)
+	for i, char := range result {
+		if unicode.IsLetter(char) {
+			val := shift(v[i%keyLen] - 'a')
+			new += shift(val).Encode(string(char))
+		}
+	}
+	return new
+}
+
+func (v vigenere) Decode(input string) (new string) {
+	key := strings.ToLower(strings.ReplaceAll(string(v), " ", ""))
+	keyLen := len(key)
+	for i, char := range input {
+		if unicode.IsLetter(char) {
+			val := shift(key[i%keyLen] - 'a')
+			deshift := shift(-int(val))
+			new += deshift.Encode(string(char))
+		}
+	}
+	return new
+}
+
+func main() {
+	// s := "ab1 zyx 5678,"
+	// fmt.Println(Cipher(s, 26))
+	// fmt.Println(Atbash(s))
+	// t := shift(2)
+	// fmt.Println(t. Encode(s))
+
+	key := vigenere("qgbvno")
+	fmt.Println(key.Encode("cof-FEE,_123!"))
 
 }
