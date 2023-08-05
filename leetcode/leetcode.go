df717f1b740487e4997e5dae706b4fecc598c29e
mramirez92 <ramirez.92.monica@gmail.com>
2023-08-05 17:18:59 -0500

    back

---
+++ leetcode/leetcode.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func IsSubsequence(s, t string) bool {
+	i := 0
+	for _, char := range t {
+		if i < len(s) && byte(char) == s[i] {
+			i++
+		}
+	}
+	return i == len(s)
+}
+
+func FindPivot(nums []int) int {
+	left, right := 0, 0
+
+	for _, num := range nums {
+		right += num
+	}
+
+	for i, num := range nums {
+		right -= num
+		if right == left {
+			return i
+		}
+		left += num
+	}
+	return -1
+}
+
+func MergeStrAlt(word1, word2 string) (merged string) {
+	remainder, lenA, lenB := "", len(word1), len(word2)
+	if lenA > lenB {
+		remainder = word1[lenB:]
+	} else {
+		remainder = word2[lenA:]
+	}
+
+	for i, char := range word1 {
+		if i < lenB {
+			merged += string(char) + string(word2[i])
+		}
+	}
+	return merged + remainder
+}
+
+func removeDuplicates(nums []int) int {
+	seen := map[int]struct{}{}
+	// unique := []int{}
+	for _, num := range nums {
+		if _, ok := seen[num]; !ok {
+			seen[num] = struct{}{}
+		}
+	}
+	return len(seen)
+}
+
+func remD(nums []int) int {
+	seen := nums[0]
+	count := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != seen {
+			nums[count] = nums[i]
+			count++
+		}
+		seen = nums[i]
+	}
+	return count
+}
+
+func flower(spaces []int, new int) bool {
+	count := 0
+	for i := 0; i < len(spaces); i++ {
+		if spaces[i] == 0 && (i == 0 || spaces[i-1] == 0) && (i == len(spaces)-1 || spaces[i+1] == 0) {
+			count++
+			spaces[i] = 1
+		}
+	}
+	return count >= new
+}
+
+func MaxSub(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	max, current := nums[0], nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		if current+nums[i] > nums[i] {
+			current += nums[i]
+		} else {
+			current = nums[i]
+		}
+		if current > max {
+			max = current
+		}
+	}
+	return max
+}
+
+func maxPro(prices []int) (profit int) {
+	minPrice := prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if prices[i]-minPrice > profit {
+			profit = prices[i] - minPrice
+		}
+	}
+	return
+}
+
+func sorted(n []int) (max int) {
+	max = n[0]
+	for _, num := range n {
+		if num > max {
+			max = num
+		}
+	}
+	return
+}
+
+func candy(nums []int, add int) []bool {
+	max := sorted(nums)
+
+	greater := []bool{}
+
+	for _, num := range nums {
+		if num+add >= max {
+			greater = append(greater, true)
+		} else {
+			greater = append(greater, false)
+		}
+	}
+	return greater
+}
+
+// *helper function for reversing vowel in string
+func isVowel(char byte) bool{
+	char = strings.ToLower(string(char))[0]
+	return char == 'a' || char == 'e' || char == 'o' || char == 'i' || char == 'u'
+}
+
+func RevVowels(s string) string{
+	str := []byte(s)
+	i, j := 0, len(s)-1
+
+	for i < j{
+		if !isVowel(str[i]){
+			i++
+		}else if !isVowel(str[j]){
+			j--
+		}else {
+			str[i], str[j] = str[j], str[i]
+			i++
+			j++
+		}
+	}
+	return string(str)
+}
+
+func main() {
+	nums := []int{2, 3, 5, 1, 3}
+	fmt.Println(candy(nums, 3))
+	fmt.Println(sorted(nums))
+	str := "greAt"
+	fmt.Println(RevVowels(str))
+
+}
---
+++ leetcode/leetcode_test.go
@@ -0,0 +1,60 @@
+package leetcode
+
+import "testing"
+
+func TestIsSub(t *testing.T){
+	cases := [] struct {
+		subsequence string
+		testString string
+		expected bool
+	}{
+		{
+			subsequence: "abc",
+			testString: "anbnc",
+			expected: true,
+		},
+		{
+			subsequence: "aa",
+			testString: "bb",
+			expected: false,
+		},
+		{
+			subsequence: "monica",
+			testString: "zmyxonwica",
+			expected: true,
+		},
+	}
+	for _, tc := range cases{
+		results := IsSubsequence(tc.subsequence,tc.testString)
+		if results != tc.expected{
+			t.Errorf("got%+v, expected %+v", results, tc.expected)
+		}
+	}
+}
+
+func TestFindPivot(t *testing.T) {
+	cases := [] struct {
+		nums []int
+		expected int
+	}{
+		{
+			nums: []int {5, 2, 3, 4, 3}, 
+			expected: 2,
+		},
+		{
+			nums: []int {2, 1, -1}, 
+			expected: 0,
+		},
+		{
+			nums: []int {2,7},
+			expected: -1,
+		},
+	}
+	for _, tc := range cases{
+		results := FindPivot(tc.nums)
+		if results != tc.expected { 
+			t.Errorf("got %+v, expected %+v", results, tc.expected)
+		}
+	}
+
+}
