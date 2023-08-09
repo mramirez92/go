aafa55c304af1a098033eb3e49eca39807d4f59a
mramirez92 <ramirez.92.monica@gmail.com>
2023-08-09 15:52:33 -0500

    practice

--- leetcode/strings/str.go
+++ leetcode/strings/str.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func stringComprehension(chars []byte) (int, []byte) {
@@ -26,13 +27,30 @@ func stringComprehension(chars []byte) (int, []byte) {
 	return rewriteIdx, chars
 }
 
+func revWords(s string)string{
+	words := strings.Fields(s)
+	last := len(words)-1
+	for i:= 0; i <len(words)/2;i++{
+		words[i], words[last-i] = words[last-i], words[i]
+		}
+	str := words[0]
+	for i := 1; i < len(words); i++{
+		str += " "+ words[i]
+	}
+	return str
+}
+
 func main() {
-	b := []byte{'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'a', 'a'}
+	// b := []byte{'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'a', 'a'}
 	// longBoi := []byte{'p','p','p','p','m','m','b','b','b','b','b','u','u','r','r','u','n','n','n','n','n','n','n','n','n','n','n','u','u','u','u','a','a','u','u','r','r','r','s','s','a','a','y','y','y','g','g','g','g','g'}
 
 	// ba := []byte{'b','b','b','a','a','a','a','c','c'}
 
-	num, arr := stringComprehension(b)
-	fmt.Println(string(arr[:num]))
+	// num, arr := stringComprehension(b)
+	// fmt.Println(string(arr[:num]))
+
+	rev := revWords("   open    door  prince  cats")
+	fmt.Println(rev)
+
 
 }
