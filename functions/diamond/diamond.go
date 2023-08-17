5b2e862bba2b2482f5f2d2742c1c23cd51293206
mramirez92 <ramirez.92.monica@gmail.com>
2023-08-17 15:39:30 -0500

    refactored function for clarity' '

--- functions/diamond/diamond.go
+++ functions/diamond/diamond.go
@@ -1,47 +1,43 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strings"
-	"errors"
 )
 
 func Diamond(char byte) (string, error) {
-    if char <'A' || char > 'Z'{
-        return "", errors.New("out of range byte")
-    }
+	if char < 'A' || char > 'Z' {
+		return "", errors.New("out of range byte")
+	}
 	if char == 'A' {
 		return string(rune('A')), nil
-    }
-	middle := int(char)
+	}
 
-	n := (middle-64)*2 - 1
+	width := (int(char)-64)*2 - 1
 
-	l, r, c := 0, 0, (n - 2)
+	l, r, c := 0, 0, (width - 2)
 
 	letters := []string{}
 
-	for i := middle; i >= 65; i-- {
-		if i == 65 {
-			row := strings.Repeat(" ", (n-1)/2) + string(rune(i)) + strings.Repeat(" ", (n-1)/2)
-			letters = append([]string{row}, letters...)
+	for i := char; i >= 'A'; i-- {
+		letter := string(rune(i))
+		if i == 'A' {
+			row := strings.Repeat(" ", (width-1)/2) + letter + strings.Repeat(" ", (width-1)/2)
+			letters = append([]string{row}, append(letters, row)...)
 		} else {
-			row := strings.Repeat(" ", l) + string(rune(i)) + strings.Repeat(" ", c) + string(rune(i)) + strings.Repeat(" ", r)
+			row := strings.Repeat(" ", l) + letter + strings.Repeat(" ", c) + letter + strings.Repeat(" ", r)
 			letters = append([]string{row}, letters...)
-			l++
-			r++
-			c -= 2
+			if i != char {
+				letters = append(letters, row)
+			}
+			l, r, c = l+1, r+1, c-2
 		}
-	}
-
-	for i := len(letters) - 2; i >= 0; i-- {
-		letters = append(letters, letters[i])
 	}
 	return strings.Join(letters, "\n"), nil
 }
 
-func main(){
-	fmt.Println(Diamond('c'))
+func main() {
+	fmt.Println(Diamond('M'))
 
-	
 }
